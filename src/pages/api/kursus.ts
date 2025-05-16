b8e24782aecbcebda47c438ecf44285893c41f8e
FathanOswaldy <mvigilante112@gmail.com>
2025-05-16 21:40:39 +0700

    mongodb

---
+++ src/pages/api/kursus.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import clientPromise from "../../lib/mongodb";
+
+type ResponseData = {
+  message: string
+}
+ 
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  const client = await clientPromise;
+  const db = client.db("elearning");
+  switch (req.method) {
+    case "GET":
+      const kursus = await db.collection("kursus").find({}).toArray();
+      res.json({ status: 200, data: kursus });
+      break;
+  }
+}
