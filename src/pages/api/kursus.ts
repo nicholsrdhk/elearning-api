dfda6a26e808e40af62f32f378392e60468220a2
FathanOswaldy <mvigilante112@gmail.com>
2025-06-20 19:56:06 +0700

    terbaru 2

--- src/pages/api/kursus.ts
+++ src/pages/api/kursus.ts
@@ -7,7 +7,7 @@ export default async function handler(
 ) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Header', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   const client = await clientPromise;
   const db = client.db("elearning");
